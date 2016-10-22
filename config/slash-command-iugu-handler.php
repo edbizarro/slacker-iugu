3a320500e81fac84fabd99cecd02e74e1498ecf5
Eduardo Bizarro <edbizarro@gmail.com>
2016-10-22 19:54:54 -0200

    new config merged with spatie\'s config

--- config/slash-command-iugu-handler.php
+++ config/slash-command-iugu-handler.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
-
     /*
      * The token provided by iugu.
      */
     'token' => env('IUGU_API_TOKEN'),
+    'handlers' => [
+        \Edbizarro\Slacker\Iugu\Handlers\IuguCustomerHandler::class,
+    ],
 ];
